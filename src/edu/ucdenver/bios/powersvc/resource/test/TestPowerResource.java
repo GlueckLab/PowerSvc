c16c4471405a3a163085d16230facc0b2259b6be
Sarah Kreidler <sarah5280@gmail.com>
2012-12-10 04:16:30 +0000

    added test case for sample size

--- src/edu/ucdenver/bios/powersvc/resource/test/TestPowerResource.java
+++ src/edu/ucdenver/bios/powersvc/resource/test/TestPowerResource.java
@@ -27,9 +27,33 @@ import junit.framework.TestCase;
 
 import org.restlet.resource.ClientResource;
 
+import edu.ucdenver.bios.powersvc.application.PowerConstants;
 import edu.ucdenver.bios.powersvc.resource.PowerResource;
+import edu.ucdenver.bios.powersvc.resource.PowerServerResource;
+import edu.ucdenver.bios.powersvc.resource.SampleSizeResource;
+import edu.ucdenver.bios.powersvc.resource.SampleSizeServerResource;
+import edu.ucdenver.bios.webservice.common.domain.BetaScale;
+import edu.ucdenver.bios.webservice.common.domain.BetweenParticipantFactor;
+import edu.ucdenver.bios.webservice.common.domain.Category;
+import edu.ucdenver.bios.webservice.common.domain.Covariance;
+import edu.ucdenver.bios.webservice.common.domain.Hypothesis;
+import edu.ucdenver.bios.webservice.common.domain.HypothesisBetweenParticipantMapping;
+import edu.ucdenver.bios.webservice.common.domain.NamedMatrix;
+import edu.ucdenver.bios.webservice.common.domain.NominalPower;
 import edu.ucdenver.bios.webservice.common.domain.PowerResult;
+import edu.ucdenver.bios.webservice.common.domain.ResponseNode;
+import edu.ucdenver.bios.webservice.common.domain.SampleSize;
+import edu.ucdenver.bios.webservice.common.domain.SigmaScale;
+import edu.ucdenver.bios.webservice.common.domain.StandardDeviation;
+import edu.ucdenver.bios.webservice.common.domain.StatisticalTest;
 import edu.ucdenver.bios.webservice.common.domain.StudyDesign;
+import edu.ucdenver.bios.webservice.common.domain.TypeIError;
+import edu.ucdenver.bios.webservice.common.enums.CovarianceTypeEnum;
+import edu.ucdenver.bios.webservice.common.enums.HypothesisTrendTypeEnum;
+import edu.ucdenver.bios.webservice.common.enums.HypothesisTypeEnum;
+import edu.ucdenver.bios.webservice.common.enums.SolutionTypeEnum;
+import edu.ucdenver.bios.webservice.common.enums.StatisticalTestTypeEnum;
+import edu.ucdenver.bios.webservice.common.enums.StudyDesignViewTypeEnum;
 
 /**
  * Test class to connect to a local instance of the power service
@@ -38,10 +62,10 @@ import edu.ucdenver.bios.webservice.common.domain.StudyDesign;
  */
 public class TestPowerResource extends TestCase
 {
-    // Create the client resource to connect to the power service
-    ClientResource clientResource = null; 
+    // Create the client resources 
     PowerResource powerResource = null;
-
+    SampleSizeResource sampleSizeResource = null;
+    
     /**
      * Connect to the server
      */
@@ -49,9 +73,9 @@ public class TestPowerResource extends TestCase
     {
         try
         {
-            clientResource = new ClientResource("http://localhost:8080/power/power"); 
             //clientResource = new ClientResource("http://sph-bi-sakhadeo:8080/power/power"); 
-            powerResource = clientResource.wrap(PowerResource.class);
+            powerResource = new PowerServerResource();
+            sampleSizeResource = new SampleSizeServerResource();
         }
         catch (Exception e)
         {
@@ -83,6 +107,30 @@ public class TestPowerResource extends TestCase
         }
     }
 
+    /**
+     * Calculate samplesize for a one-sample t test
+     */
+    public void testSampleSize()
+    {
+        StudyDesign studyDesign = buildDesignOneSample();
+        // calculate power
+        try
+        {
+            ArrayList<PowerResult> powerList = sampleSizeResource.getSampleSize(studyDesign);
+            for(PowerResult power : powerList)
+            {
+                System.out.println(power.toXML());
+            }
+            assertTrue(true);
+        }
+        catch (Exception e)
+        {
+            System.err.println("testPower Failed to retrieve: " + e.getMessage());
+            fail();
+        }
+    }
+    
+    
     /**
      * Create a StudyDesign object representing a basic t-test design
      * 
@@ -128,5 +176,82 @@ public class TestPowerResource extends TestCase
 
 
         return studyDesign;
+    }
+    
+    /**
+     * Create a StudyDesign object representing a basic t-test design
+     * 
+     * @return StudyDesign 
+     */
+    private StudyDesign buildDesignOneSample()
+    {          
+        StudyDesign studyDesign = new StudyDesign();
+        studyDesign.setViewTypeEnum(StudyDesignViewTypeEnum.GUIDED_MODE);
+        studyDesign.setSolutionTypeEnum(SolutionTypeEnum.SAMPLE_SIZE);
+        studyDesign.setName("One sample t test");
+
+        ArrayList<BetaScale> betaScaleList = new ArrayList<BetaScale>();
+        betaScaleList.add(new BetaScale(1));
+        studyDesign.setBetaScaleList(betaScaleList);
+        
+        ArrayList<NominalPower> nominalPowerList = new ArrayList<NominalPower>();
+        nominalPowerList.add(new NominalPower(0.9));
+        studyDesign.setNominalPowerList(nominalPowerList);
+
+        // add a test 
+        ArrayList<StatisticalTest> testList = new ArrayList<StatisticalTest>();
+        testList.add(new StatisticalTest(StatisticalTestTypeEnum.HLT));
+        studyDesign.setStatisticalTestList(testList);
+
+        // add alpha values
+        ArrayList<TypeIError> alphaList = new ArrayList<TypeIError>();
+        alphaList.add(new TypeIError(0.05));
+        studyDesign.setAlphaList(alphaList);
+
+        // add sigma scale values
+        ArrayList<SigmaScale> sigmaScaleList = new ArrayList<SigmaScale>();
+        sigmaScaleList.add(new SigmaScale(1));
+        studyDesign.setSigmaScaleList(sigmaScaleList);
+        
+        // build the hypotheses
+        Hypothesis hypothesis = new Hypothesis();
+//        hypothesis.setType(HypothesisTypeEnum.MAIN_EFFECT);
+        hypothesis.setType(HypothesisTypeEnum.GRAND_MEAN);
+        studyDesign.setHypothesisToSet(hypothesis);
+        // set theta null
+        double[][]  thetaNullData = {{0}};
+        NamedMatrix thetaNull = new NamedMatrix(PowerConstants.MATRIX_THETA_NULL);
+        thetaNull.setDataFromArray(thetaNullData);
+        thetaNull.setRows(1);
+        thetaNull.setColumns(1);
+        studyDesign.setNamedMatrix(thetaNull);
+        
+        // build beta matrix
+        double [][] betaData = {{2}};
+        NamedMatrix beta = new NamedMatrix(PowerConstants.MATRIX_BETA);
+        beta.setDataFromArray(betaData);
+        beta.setRows(1);
+        beta.setColumns(1);
+        studyDesign.setNamedMatrix(beta);
+
+        // build response variables list
+        ArrayList<ResponseNode> responseList = new ArrayList<ResponseNode>();
+        responseList.add(new ResponseNode("outcome"));
+        studyDesign.setResponseList(responseList);
+        
+        // build covariance
+        Covariance covar = new Covariance();
+        covar.setType(CovarianceTypeEnum.UNSTRUCTURED_CORRELATION);
+        covar.setName(PowerConstants.RESPONSES_COVARIANCE_LABEL);
+        ArrayList<StandardDeviation> stdDevList = new ArrayList<StandardDeviation>();
+        stdDevList.add(new StandardDeviation(3));
+        double [][] sigmaData = {{1}};
+        covar.setRows(1);
+        covar.setColumns(1);
+        covar.setBlobFromArray(sigmaData);
+        covar.setStandardDeviationList(stdDevList);
+        studyDesign.addCovariance(covar);
+        return studyDesign;
+
     }
 }
